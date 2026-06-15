475e0d26d91e71acb7289b17414b63ad9bed34d1
ABDULRAHMAN SAADALLAH <159337457+ABDULRAHMAN-ALSAADI@users.noreply.github.com>
2026-06-15 14:54:35 +0300

    Delete RUN_CONTROL_CENTER.bat

--- RUN_CONTROL_CENTER.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-title Robot Cell Factory Supervisor - Persistent Sorter
-echo Starting corrected coordinator from:
-echo %CD%
-python -m pip install -r requirements.txt
-python cell_control_center.py
-pause
